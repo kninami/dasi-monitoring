1a507e3c2d6a0702bbab425e3173834d91b1c04c
Cho kyungsuk <kninami@gmail.com>
2024-09-11 17:13:59 +0900

    트위터계정 확인  프로그램 개발

---
+++ twitter_checker.py
@@ -0,0 +1,60 @@
+import json
+import os
+import requests
+from dotenv import load_dotenv
+import sheet_processor
+
+load_dotenv()
+
+json_keyfile_path = os.getenv("KEYFILE_PATH")
+spreadsheet_id = os.getenv("TWITTER_SHEET_ID")
+bearer_token = os.getenv("BEARER_TOKEN")
+
+# Replace with your actual Bearer Token from Twitter Developer portal
+def check_user_status(username):
+    url = f"https://api.twitter.com/2/users/by/username/{username}"
+    headers = {"Authorization": f"Bearer {bearer_token}"}
+    
+    response = requests.get(url, headers=headers)
+    response_content = response.json()
+
+    print(response_content)
+
+    # "Too Many Requests" 처리
+    if response_content.get("title") == "Too Many Requests":
+        return ""  
+    
+    # "errors" 처리
+    errors = response_content.get("errors", [])
+    for error in errors:
+        detail = error.get("detail", "")
+        if "suspended" in detail:
+            return ['미유통', '정지']
+        if "find" in detail:
+            return ['미유통', '삭제']
+    
+    # errors가 없는 경우
+    return ['유통', '해당없음']
+
+if __name__ == "__main__":
+    
+    sheet = sheet_processor.connect_to_gsheet(json_keyfile_path, spreadsheet_id)
+    worksheet = sheet.get_worksheet(0)
+    user_list = worksheet.get_all_values()
+    
+    #체크해야 할 트위터 계정 3개만 가져옴
+    need_to_check_user_rows = []
+    for index, user in enumerate(user_list):
+        user_account = user[2]
+        user_check_result = user[4]
+
+        if not user_check_result: 
+            check_result = check_user_status(user_account)
+            need_to_check_user_rows.append(user_account)
+
+            if isinstance(check_result, list) and len(check_result) >= 2:
+                worksheet.update_cell(index + 1, 5, check_result[0])  # E열 (5번째 열)에 첫 번째 값 ("미유통")을 업데이트
+                worksheet.update_cell(index + 1, 6, check_result[1])  # F열 (6번째 열)에 두 번째 값 ("정지")를 업데이트
+            else:
+                print("API limit reached")
+                break
